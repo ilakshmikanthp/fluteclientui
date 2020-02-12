36aa400d1b3ffcd28721539e2efd407ec86c183d
Lakshmikanth P <lakshmikanth.p@zen3.com-at-143912951401>
2020-02-12 16:25:42 +0530

    Minor changes

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React /*, { useState } */ from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
-import InputIcon from '@material-ui/icons/Input';
+import { AppBar, Toolbar, /* Badge, Hidden, IconButton */ } from '@material-ui/core';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
+// import InputIcon from '@material-ui/icons/Input';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
@@ -38,7 +38,7 @@ const Topbar = props => {
     marginRight: 10
   }
 
-  const [notifications] = useState([]);
+  // const [notifications] = useState([]);
 
   return (
     <AppBar
